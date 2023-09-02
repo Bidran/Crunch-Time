90dff1960d9e9634cfaf28d8efc3bea4936b572f
Bidran <brandiczvonimir@gmail.com>
2023-09-02 18:30:23 +0100

    Add a timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,24 @@
 
-
 let keyboard = document.getElementById("keyboardimg");
 
 
 
-let score = 0;
+var score = 0;
 
+/**
+ * Incrementally adds number of clicks/keypresses by 1 and adds it to current score
+ */
 function addClicks() {
     score++;
     document.getElementById("scorenum").innerHTML = score;
 };
 
-
+var timeleft = 30;
+var downloadTimer = setInterval(function(){
+  if(timeleft <= 0){
+    clearInterval(downloadTimer);
+    document.getElementById("countdown").innerHTML = "0";
+  } else 
+    document.getElementById("countdown").innerHTML = timeleft + "s";
+     timeleft -= 1;
+}, 1000);
